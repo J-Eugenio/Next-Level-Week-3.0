4cfdc8b0dcb29da680a562ff0da123fa45d7e70a
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-10-13 18:50:34 -0300

    Backend OK

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,21 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
+
 import './database/connection';
 import routes from './routes';
 
+import errorHandle from './errors/handle';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandle);
+
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
